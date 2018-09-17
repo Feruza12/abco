468902e828fb2e0c552765c9580c6fc611134ab6
Feruza12 <f.latipova@student.inha.uz>
2018-09-17 14:02:54 +0500

    second

--- app/js/common.js
+++ app/js/common.js
@@ -1,26 +1,21 @@
 
 $('.owl-one').owlCarousel({
-    stagePadding: 10,
+     stagePadding: 10,
     loop: true,
     margin: 10,
     nav: false,
-    responsiveClass: true,
     responsive:{
         0:{
             items:1,
-            nav:false,
         },
         600:{
             items:2,
-            nav:false
         },
         1000:{
             items:3,
-            nav:false
         },
         1200:{
             items:4,
-            nav:false
         }
     }
 })
@@ -75,26 +70,17 @@ $('.owl-three').owlCarousel({
     }
 })
 $(document).ready(function(){
-     $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
+     $("a").on('click', function(event) {   
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
       event.preventDefault();
-
-      // Store hash
       var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
+      }, 800, function(){       
         window.location.hash = hash;
       });
-    } // End if
+    } 
   });
 
   $.mask.definitions['9']='';
